ee70f2ec58ea03513969cd5de5ebe52ffc538d30
kelvinbex2 <obamedo20@gmail.com>
2025-01-11 15:12:28 +0100

    XXXXXX

--- src/es/etg/prog/modelo/Evento.java
+++ src/es/etg/prog/modelo/Evento.java
@@ -7,61 +7,68 @@ public class Evento {
     public Evento(int filla, int columna) {
         asientos = new Asiento[filla][columna];
         char letra = 'A';
-
         for (int i = 0; i < filla; i++) {
             for (int j = 0; j < columna; j++) {
                 asientos[i][j] = new Asiento((i + 1) + "" + (char) (j + letra));
             }
-
         }
+
     }
 
     public void mostrar() {
-        for (Asiento[] asiento : asientos) {
-            for (Asiento silla : asiento) {
-                System.out.print(silla + " ");
+
+        for (Asiento[] asientos2 : asientos) {
+            for (Asiento asiento : asientos2) {
+                System.out.print(asiento + "");
+
             }
             System.out.println();
-
         }
     }
 
     public boolean indicarPremium(String nombre, String dni, int edad) {
         for (int i = 0; i < asientos.length; i++) {
             for (int j = 0; j < asientos[i].length; j++) {
+
                 if (!asientos[i][j].ocupado) {
                     asientos[i][j].asignar(nombre, dni, edad);
-                    System.out.println("Asiento asignado " + asientos[i][j].id + " (Premium)");
+                    System.out.println("Asiento asignado " + asientos[i][j].id + " a " + nombre + " (Premium)");
                     return true;
                 }
-
             }
         }
-        System.out.println("Ya esta ocupado este asiento por lo que no se puede vender este asiento");
+        System.out.println("Asiento no disponible");
         return false;
+
     }
 
     public boolean venderEntradas(int limite, String tipo, String nombre, String dni, int edad) throws Exception {
 
         if (limite <= 0) {
-            System.out.println("Solo tiene que ser mayor");
-            return false;
+            System.out.println("Debe ser mayor que 0 ");
         }
-        int entrada = 0;
-        switch (tipo.toLowerCase()) {
-            case "p":
-                while (entrada < limite) {
+        int entradaVendidas = 0;
 
-                    if (!indicarPremium(nombre, dni, edad)) {
-                        return false;
+        try {
+            switch (tipo.toLowerCase()) {
+                case "p":
+
+                    while (entradaVendidas < limite) {
+                        if (!indicarPremium(nombre, dni, edad)) {
+                            return false;
+                        }
+                        entradaVendidas++;
+
                     }
-                    entrada++;
-                }
+                    return true;
 
-                return true;
-            default:
-                return false;
+                default:
+                    throw new Exception("No valida");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
+        return false;
     }
 }
--- src/es/etg/prog/vista/App.java
+++ src/es/etg/prog/vista/App.java
@@ -1,5 +1,11 @@
 package es.etg.prog.vista;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 import es.etg.prog.entrada.Entrada;
@@ -10,22 +16,27 @@ public class App {
         Scanner sc = new Scanner(System.in);
         int opc = 0;
         Evento evento = new Evento(5, 9);
+
         do {
-            System.out.print("1.Mostrar\n2.comprar\nEscribe: ");
-            opc = Entrada.leerInt();
-            
-            switch (opc) {
-                case 1:
-                    evento.mostrar();
-                    break;
+            try {
+                System.out.print("1.Mostrar\n2.comprar\nEscribe: ");
+                opc = Entrada.leerInt();
 
-                case 2:
+                switch (opc) {
+                    case 1:
+                        evento.mostrar();
+                        break;
 
-                    gestionar(evento);
-                    break;
-                default:
-                    throw new IndexOutOfBoundsException("Error");
+                    case 2:
 
+                        gestionar(evento);
+                        break;
+                    default:
+                        throw new IndexOutOfBoundsException("Error no exite ese numero ");
+
+                }
+            } catch (IndexOutOfBoundsException e) {
+                e.printStackTrace();
             }
         } while (opc != 0);
 
@@ -36,6 +47,10 @@ public class App {
         String dni = Entrada.leerString();
         System.out.print("Tipo: ");
         String tipo = Entrada.leerString();
+        while (!tipo.equals("p")) {
+            System.out.print("Debe ser p/l: ");
+            tipo = Entrada.leerString();
+        }
 
         System.out.print("Nombre: ");
         String nombre = Entrada.leerString();
@@ -48,11 +63,48 @@ public class App {
 
         if (evento.venderEntradas(limite, tipo, nombre, dni, edad)) {
             System.out.println("Venta completa");
+            guardaTicket(nombre, dni, edad, limite);
 
         } else {
             System.out.println("Venta no correcta");
         }
 
+    }
+
+    public static void guardaTicket(String nombre, String dni, int edad, int entradas) {
+        File ficFile = new File("Ticket.txt");
+        int ticketNumber = contarTickets(ficFile) + 1;
+        try (BufferedWriter fw = new BufferedWriter(new FileWriter(ficFile, true))) {
+            fw.write("**********Ticket " + ticketNumber + "***********" + System.lineSeparator());
+            fw.write("Nombre: " + nombre + System.lineSeparator());
+            fw.write("Dni: " + dni + System.lineSeparator());
+            fw.write("Edad: " + edad + System.lineSeparator());
+            fw.write("Entradas Compardo: " + entradas + System.lineSeparator());
+            fw.write("***************************" + System.lineSeparator());
+            fw.write(System.lineSeparator());
+            fw.write(System.lineSeparator());
+
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int contarTickets(File ficFile) {
+        int count = 0;
+        String line = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(ficFile))) {
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("**********Ticket")) {
+                    count++;
+                }
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return count;
     }
 
 }
